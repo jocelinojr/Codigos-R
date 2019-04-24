e05d23af8640cb6c93bf50ad23cf80e214bc227c
Jocelino Mendes da Silva Júnior <34030698+jocelinojr@users.noreply.github.com>
2019-04-24 16:58:59 -0300

    Add files via upload

--- analise_empresas_serv.R
+++ analise_empresas_serv.R
@@ -12,21 +12,6 @@ conecta_labcontas <- function(alias){
 
 }
 
-#-------------------------------------
-# Limpa os caracteres estranhos num cpf
-limpa_cpf <- function(cpf){
-  str_remove_all(string = cpf, pattern = "[*\\.\\-]")  
-}
-
-nomes <- NULL
-
-
-# 'Varre a lista de nomes dos servidores PB e monta um set para o SQL
-monta_sql_nomes <- function(x){
-  nome <- x[2]
-  nomes <- str_c(nomes, nome, sep = "'")
-
-}
 
 # ---------------------------------------------------------------------
 # VERSÃO OBSOLETA
@@ -59,18 +44,17 @@ monta_sql_nomes <- function(x){
 #   
 # } # func
 
-
 #--------------------------------------------------------
 # Vai até o banco dos servidores fornecido pela CGU e pega os CPFs
 # completos dos servidores federais na Paraíba
-pega_cpfs <- function(nomes_servidores, conexao, tabela){
+pega_cpfs <- function(nomes_servidores, conexao, tabela, campos_select, campo_where){
   
   # seta a opção de quotes para o apóstrofo simples para funcionar no SQL Server
   options(useFancyQuotes = FALSE)
   nomes <- str_c(sQuote(nomes_servidores), collapse = ',')
   nomes <- str_c("(", nomes, ")")
-  sql <- str_c("SELECT CO_CPF, NOME_ORIGEM FROM [BD_SERVIDOR].[dbo].", tabela)
-  sql <- paste(sql,"WHERE NOME_ORIGEM IN",nomes)
+  sql <- str_c("SELECT", campos_select, "FROM", tabela, sep=" ")
+  sql <- paste(sql,"WHERE", campo_where ,"IN",nomes)
   resultado <- dbSendQuery(conexao, sql)
   serv_cpf <- dbFetch(resultado)
   # libera os recursos da query
@@ -97,11 +81,16 @@ con <- conecta_labcontas("LABCONTAS")
 # retira os caracteres estranhos do CPF
 serv_pb$CPF <- str_remove_all(serv_pb$CPF, pattern = "[*\\.\\-]")
 
+
 # A partir da lista de nomes dos servidores federais na PB vai à tabela de servidores da CGU no LABCONTAS e obtém o CPF completo deles
-serv_pb_cpfs <- pega_cpfs(serv_pb$NOME, conexao = con, tabela = "[SERVIDOR_CGU]")
+serv_pb_cpfs <- pega_cpfs(serv_pb$NOME, 
+                          conexao = con, 
+                          tabela = "[BD_SERVIDOR].[dbo].[SERVIDOR_CGU]",
+                          campos_select = "CO_CPF, NOME_ORIGEM",
+                          campo_where = "NOME_ORIGEM")
 
 # remove as duplicidades
-serv_pb_cpfs <- distinct(serv_pb_cpfs)
+View(serv_pb_cpfs) <- distinct(serv_pb_cpfs)
 
 
 # verifica se temos algum CPF que não tenha 11 dígitos, assim podemos pegar um pedaço do CPF
@@ -113,20 +102,59 @@ if (!any(nchar(serv_pb_cpfs$CO_CPF) != 11)) {
 glimpse(serv_pb_cpfs)
 
 
-# junta os datasets 
-serv_pb_completo <-  merge(serv_pb, serv_pb_cpfs, by.x = c("CPF", "NOME"), by.y=c("CPF_PARTE", "NOME_ORIGEM"))
+# junta os datasets (left join)
+# Left join para preservar todos os servidores da PB
+serv_pb_join_cgu <-  merge(serv_pb, serv_pb_cpfs, by.x = c("CPF", "NOME"), by.y=c("CPF_PARTE", "NOME_ORIGEM"), all.x=TRUE)
+
+# verifica que há uma diferença entre os datasets após a junção 
+nrow(serv_pb_cpfs) - nrow(serv_pb_join_cgu)
+
+# filtra todos os que não foram encontrados o CPF
+serv_pb_sem_cpf <- filter(serv_pb_join_cgu, is.na(CO_CPF))
+
+
+# Vai no LABCONTAS na tabela de CPF buscar os CPFs desses mais de 2.000 que estão sem
+serv_pb_novos_cpfs <- pega_cpfs(serv_pb_sem_cpf$NOME, 
+                                con, 
+                                "[BD_RECEITA].[dbo].[CPF]",
+                                "NUM_CPF, NOME",
+                                "NOME")
+
+
+View(serv_pb_novos_cpfs)
+
+# Cria uma coluna que contém uma parte do CPF para poder fazer o join com os servidores da PB que ainda não temos o CPF
+serv_pb_novos_cpfs$CPF_PARTE <- substr(serv_pb_novos_cpfs$NUM_CPF, 4, 9)
+
+
+# junta os datasets (left join)
+# Left join para preservar todos os servidores da PB
+serv_pb_join_cpf_receita <-  merge(serv_pb_sem_cpf, serv_pb_novos_cpfs, by.x = c("CPF", "NOME"), by.y=c("CPF_PARTE", "NOME"), all.x=TRUE)
+
+nrow(serv_pb_join_cpf_receita)
+nrow(serv_pb_join_cgu)
+
+apenas_cpf <-  filter(serv_pb_join_cgu, !is.na(CO_CPF))
+apenas_cpf <-  rename(apenas_cpf, NUM_CPF=CO_CPF)
+
+# pega apenas os que tem CPF e junta com os que acabamos de pegar no banco da Receita
+serv_pb_completo <- rbind(apenas_cpf, serv_pb_join_cpf_receita)
+
+
+servidore_orgao <- serv_pb_completo %>% 
+  group_by(ORG_LOTACAO) %>%
+  summarize(qde=n())
+
+write_csv(servidore_orgao, "servidores_pb_por_orgao.csv")
+
+head(servidore_orgao, 200)
+
+filter(serv_pb_completo, is.na(NUM_CPF))
 
 View(serv_pb_completo)
-View(serv_federais)
-
-glimpse(serv_pb_cpfs)
-
-
-
-View(filter(serv_federais,  grepl("^ROBERTO MAIA",NOME)))
-filter(serv_pb,  grepl("^SINVAL",NOME))
-
-
+rm(serv_pb_join_final)
+View(serv_pb_join_cgu)
+view(serv_pb_sem_cpf)
 
 
 # OBSOLETO
